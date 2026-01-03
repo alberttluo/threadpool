e0d291641b9eb3ee47b9785f6bae689374dce916
Albert Luo <albertlu@ece000.ece.local.cmu.edu>
2026-01-03 18:06:18 -0500

    test: Add Makefile and add dummy task testing

--- test/threadPool_test.c
+++ test/threadPool_test.c
@@ -1,8 +1,20 @@
 #include "../threadPool.h"
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
 
-#define MAX_THREADS (100UL)
+#define MAX_THREADS (20UL)
+
+typedef struct {
+  int num1;
+  int num2;
+} dummy_t;
+
+static void *dummyTask(void *args) {
+  dummy_t *dummyArgs = (dummy_t *)args;
+
+  return (void *)(intptr_t)(dummyArgs->num1 + dummyArgs->num2);
+}
 
 /*
 * Test that initialization and freeing works on arbitrary number of threads.
@@ -23,10 +35,34 @@ static void initTest(size_t maxThreads) {
     printf("Freeing thread pool with %zu threads...\n\n", i);
     threadPoolFree(tpArray[i]);
   }
+
+  free(tpArray);
+}
+
+/*
+* Test that tasks are done successfully.
+*/
+static void taskTest(size_t numTasks) {
+  printf("Running task testing...\n\n");
+
+  threadPool_t *tp = threadPoolInit(MAX_THREADS);
+
+  // Try just dummy tasks.
+  for (size_t i = 0; i < numTasks; i++) {
+    dummy_t *args = malloc(sizeof(dummy_t));
+    assert(args != NULL);
+
+    args->num1 = i;
+    args->num2 = i + 1;
+    threadPoolAddTask(tp, dummyTask, (void *)args, free);
+  }
+
+  threadPoolFree(tp);
 }
 
 static void runTests() {
-  initTest(MAX_THREADS);
+  initTest(1UL);
+  taskTest(2UL);
 
   printf("All tests passed!");
   exit(EXIT_SUCCESS);
